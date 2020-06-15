623a980b1903db45bf0dea55ba90284e52bfd160
Peter Lam <Piitaa1234@gmail.com>
2020-06-15 18:29:21 -0400

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-My Profile that was created with React
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
---
+++ precache-manifest.c37588c904e2538ce9533974110485ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca87c4ad23727717767dc6469c05aab",
+    "url": "//github.com/plam1234/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "150ff50abe9cfbda9432",
+    "url": "//github.com/plam1234/my-react-portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "4fefd0c5ff43af5c3efa",
+    "url": "//github.com/plam1234/my-react-portfolio/static/css/main.4cba123b.chunk.css"
+  },
+  {
+    "revision": "150ff50abe9cfbda9432",
+    "url": "//github.com/plam1234/my-react-portfolio/static/js/2.69ab2f39.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "//github.com/plam1234/my-react-portfolio/static/js/2.69ab2f39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fefd0c5ff43af5c3efa",
+    "url": "//github.com/plam1234/my-react-portfolio/static/js/main.df01aa3a.chunk.js"
+  },
+  {
+    "revision": "5dfd457f663e33add5c2",
+    "url": "//github.com/plam1234/my-react-portfolio/static/js/runtime-main.a316a307.js"
+  },
+  {
+    "revision": "c6c5a902bc0feef3d04343a26dfa9570",
+    "url": "//github.com/plam1234/my-react-portfolio/static/media/github.c6c5a902.png"
+  }
+]);
